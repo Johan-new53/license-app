b577baeb2472336cc07f3872908c3d226cf94da5
johan2429 <johan@siloamhospitals.com>
2025-11-04 16:48:22 +0700

    menyelesaikan halaman about

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
