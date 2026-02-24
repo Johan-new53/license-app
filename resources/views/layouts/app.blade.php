7f964213487bf0a9529c1c96d6565b2f991f833a
J-nes <jnes1995@gmail.com>
2026-02-24 11:26:01 +0700

    bug fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,16 +85,15 @@
                                     <a class="dropdown-item" href="{{ route('hardcopys.index') }}">
                                         <i class="fa-solid fa-book-bookmark fa-lg"></i> Hard Copy
                                     </a>
-                                    
-                                    @endcan  
-                                     @can('softcopy-list')
-                                    <a class="dropdown-item" href="{{ route('softcopys.index') }}">                                        
+                                    @endcan
+
+                                    @can('softcopy-list')
+                                    <a class="dropdown-item" href="{{ route('softcopys.index') }}">
                                         <i class="fa-solid fa-book-bookmark fa-lg"></i> Soft Copy
                                     </a>
-                                    @endcan     
-
                                     @endcan
-                                      @can('finance-import')
+
+                                    @can('finance-import')
                                       <a class="dropdown-item" href="{{ route('import') }}">
                                           <i class="fa-solid fa-book-bookmark fa-lg"></i> Import
                                       </a>
